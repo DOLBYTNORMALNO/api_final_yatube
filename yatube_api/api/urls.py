2a62e54d569cc773b1fe21f4b1cfb348b018a384
Григорий Серов <grisha200170@gmail.com>
2023-07-24 18:38:18 +0300

    try

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import FollowViewSet
+
+router = DefaultRouter()
+router.register(r'follow', FollowViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,18 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets, permissions
+from .serializers import FollowSerializer
+from posts.models import Follow
+
+class FollowViewSet(viewsets.ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.follower.all()
+
+    def perform_create(self, serializer):
+        if self.request.user != serializer.validated_data['following']:
+            serializer.save(user=self.request.user)
+        else:
+            raise serializers.ValidationError("You cannot follow yourself.")
