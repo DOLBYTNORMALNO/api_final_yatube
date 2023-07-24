6554bd5cfd38e8d3dbfe2e11772681dffccf6dde
Григорий Серов <grisha200170@gmail.com>
2023-07-24 20:22:25 +0300

    try

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -3,6 +3,11 @@ from django.db import models
 
 User = get_user_model()
 
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
 
 class Post(models.Model):
     text = models.TextField()
@@ -11,6 +16,8 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name='posts')
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
+    group = models.ForeignKey(
+        Group, on_delete=models.SET_NULL, null=True, blank=True, related_name='posts')
 
     def __str__(self):
         return self.text
@@ -34,7 +41,4 @@ class Follow(models.Model):
         unique_together = ['user', 'following']
 
 
-class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+
