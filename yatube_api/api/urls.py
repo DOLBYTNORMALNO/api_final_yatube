afad9196531bb9294f44c3c5c403d8ac82268e53
Григорий Серов <grisha200170@gmail.com>
2023-07-26 17:36:25 +0300

    try

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 from .views import PostViewSet, FollowViewSet, CommentViewSet, GroupViewSet
 
 router = DefaultRouter()
@@ -10,4 +11,7 @@ router.register(r'groups', GroupViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/', obtain_jwt_token),
+    path('token/refresh/', refresh_jwt_token),
+    path('token/verify/', verify_jwt_token),
 ]
